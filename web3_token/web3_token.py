38908b09e26ecc56e22bdc366c8af94df8d682f6
ergossteam <ergossteam@gmail.com>
2023-06-08 22:21:08 +0600

    feat: add supporting web3 v6.0.0+

--- web3_token/web3_token.py
+++ web3_token/web3_token.py
@@ -9,29 +9,29 @@ from web3.auto import w3
 class Web3Token:
     def __init__(self, token: str):
         json_data = json.loads(base64.b64decode(token))
-        self.signature = json_data['signature']
-        body_parts = json_data['body'].split('\n\n')
+        self.signature = json_data["signature"]
+        body_parts = json_data["body"].split("\n\n")
         self.message = body_parts[-1]
         self.statement = body_parts[0] if len(body_parts) > 1 else None
-        self.__body_hash = None
+        self.__signable_message = None
         self.__signer = None
 
     @property
     def body(self) -> str:
         if self.statement is not None:
-            return self.statement + '\n\n' + self.message
+            return self.statement + "\n\n" + self.message
         return self.message
 
     @property
-    def body_hash(self) -> 'eth_account.messages.HexBytes':
+    def signable_message(self) -> "eth_account.messages.HexBytes":
         """
         Convert the body into a message hash, to be signed.
         """
-        if self.__body_hash is None:
-            self.__body_hash = eth_account.messages.defunct_hash_message(
+        if self.__signable_message is None:
+            self.__signable_message = eth_account.messages.encode_defunct(
                 text=self.body
             )
-        return self.__body_hash
+        return self.__signable_message
 
     def get_signer(self, validate: bool = True) -> str:
         """
@@ -39,12 +39,11 @@ class Web3Token:
         Returns address of etherium wallet
         """
         if self.__signer is None:
-            signer = w3.eth.account.recoverHash(
-                self.body_hash,
-                signature=self.signature
+            signer = w3.eth.account.recover_message(
+                self.signable_message, signature=self.signature
             )
-            if validate and not w3.isAddress(signer):
-                raise Exception(f'Invalid Token')
+            if validate and not w3.is_address(signer):
+                raise Exception(f"Invalid Token")
             self.__signer = signer
         return self.__signer
 
@@ -52,17 +51,12 @@ class Web3Token:
         """
         Get dict representation of signed message
         """
-        return dict(
-            map(
-                lambda s: s.split(': '),
-                self.message.split('\n')
-            )
-        )
+        return dict(map(lambda s: s.split(": "), self.message.split("\n")))
 
 
-if __name__ == '__main__':
-    token_ = input(f'Enter your token: ')
+if __name__ == "__main__":
+    token_ = input("Your token: ")
     web3token = Web3Token(token_)
-    print('Signer:', web3token.get_signer(validate=True))
-    print('Data:', web3token.get_data())
-    print('Statement:', web3token.statement)
+    print("Signer:", web3token.get_signer(validate=True))
+    print("Data:", web3token.get_data())
+    print("Statement:", web3token.statement)
